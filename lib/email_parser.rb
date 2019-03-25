596bb9ef285b1101f8d1f9b38b9383fe3f5ecf3a
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-25 00:32:49 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,13 @@ def parse
     .flatten.uniq
   end
 
+  emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
+  parser1 = EmailParser.new(emails1)
+  parser1.parse
 
+  emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
+  parser2 = EmailParser.new(emails2)
+  parser2.parse
 
   # Build a class EmailParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
