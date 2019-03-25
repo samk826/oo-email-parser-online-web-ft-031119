6731c0fbefdc9a1c255eb1eb8ffff29322961f28
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-25 00:36:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,13 +13,7 @@ def parse
     .flatten.uniq
   end
 
-  emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-  parser1 = EmailParser.new(emails1)
-  parser1.parse
 
-  emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-  parser2 = EmailParser.new(emails2)
-  parser2.parse
 end
 end
   # Build a class EmailParser that accepts a string of unformatted
