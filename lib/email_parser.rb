6cc1695744f9230fb0f67165150c8624bec64dbb
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-25 00:39:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,8 +13,6 @@ def parse
     .flatten.uniq
   end
 
-
-e
   # Build a class EmailParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
